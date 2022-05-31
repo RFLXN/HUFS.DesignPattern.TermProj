2fe7c7dee692391150ba5e3c8fd7422895c6018f
jhchoi123 <solid2113@naver.com>
2022-05-31 18:35:35 +0900

    add scan_file command

--- command/real/scan_file.py
+++ command/real/scan_file.py
@@ -5,16 +5,56 @@ from util.json import load_json_from_str
 
 
 class ScanFileCommand(Command):
-    def execute(self, **kwargs) -> CommandResult:
-        client = ApiClient()
-        target_endpoint = client.get_endpoint("files", "scan")
-        file_path = kwargs["file"]
+    def execute(self, *args, **kwargs) -> CommandResult:
         try:
-            f = open(file_path, "r")
-            result = client.exec_endpoint(target_endpoint, file=f)
-            f.close()
-            result = load_json_from_str(result.text)
-            return CommandResult(True, "")
-        except:
-            return CommandResult(False, "")
+            client = ApiClient()
+            target_endpoint = client.get_endpoint("files", "scan")
 
+            file_path = ""
+
+            if len(args) > 0 and args[0] is not None and args[0] != "":
+                file_path = args[0]
+
+            if kwargs["file_path"] is not None and kwargs["file_path"] != "":
+                file_path = kwargs["file_path"]
+
+            if file_path == "":
+                raise InvalidFilePathException
+
+            target_file = None
+
+            try:
+                target_file = open(file_path, "r")
+            except FileNotFoundError:
+                raise InvalidFilePathException
+
+            if target_file is None:
+                raise InvalidFilePathException
+
+            result = client.exec_endpoint(target_endpoint, file=target_file)
+
+            if result.status_code == 403:
+                raise InvalidApiKeyException
+
+            result = load_json_from_str(result.text)
+            web_url = result["permalink"]
+            resource_id = result["resource"]
+
+            return CommandResult(True, f"Successfully Upload File.\n"
+                                 + f"File Path: {file_path}\n"
+                                 + f"Scan ID: {resource_id}\n"
+                                 + f"Web URL: {web_url}")
+        except InvalidFilePathException:
+            return CommandResult(False, "Invalid File Path.")
+        except InvalidApiKeyException:
+            return CommandResult(False, "Invalid API Key.")
+
+
+class InvalidFilePathException(Exception):
+    def __init__(self):
+        super().__init__()
+
+
+class InvalidApiKeyException(Exception):
+    def __init__(self):
+        super().__init__()
